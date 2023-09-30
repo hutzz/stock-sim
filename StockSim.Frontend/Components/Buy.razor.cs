9be440a576d198135169586bdf1941022299aa41
Zach <zachhutz0@gmail.com>
2023-09-30 15:51:24 -0400

    added styling for individual buy/sell

---
+++ StockSim.Frontend/Components/Buy.razor.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Components;
+using StockSim.Frontend.Models;
+
+namespace StockSim.Frontend.Components {
+    public partial class Buy {
+        [Parameter]
+        public string? Symbol { get; set; }
+        [Parameter]
+        public EventCallback<BuySellDto> OnBuy { get; set; }
+        private int _quantity { get; set; }
+        public BuySellDto stock = new();
+        protected override void OnInitialized() {
+            Console.WriteLine(Symbol);
+        }
+        private async Task Valid() {
+            stock = new() { Symbol = Symbol, Quantity = _quantity };
+            await OnBuy.InvokeAsync(stock);
+        }
+        private void Invalid() {
+            Console.WriteLine("HOW THE FUCK");
+        }
+    }
+}
---
+++ StockSim.Frontend/Components/Sell.razor.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Components;
+using StockSim.Frontend.Models;
+
+namespace StockSim.Frontend.Components {
+    public partial class Sell {
+        [Parameter]
+        public string? Symbol { get; set; }
+        [Parameter]
+        public EventCallback<BuySellDto> OnSell { get; set; }
+        private int _quantity { get; set; }
+        public BuySellDto stock = new();
+        protected override void OnInitialized() {
+            Console.WriteLine(Symbol);
+        }
+        private async Task Valid() {
+            stock = new() { Symbol = Symbol, Quantity = _quantity };
+            await OnSell.InvokeAsync(stock);
+        }
+        private void Invalid() {
+            Console.WriteLine("HOW THE FUCK");
+        }
+    }
+}
