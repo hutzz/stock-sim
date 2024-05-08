f321ad8f4635e796e5c13b4c809b3030b73fbd8d
Zach <zachhutz0@gmail.com>
2024-05-08 15:30:00 -0400

    added breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ["./**/*.html", "./**/*.razor"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				"3xl": "1920px",
+				"4xl": "2560px",
+				"5xl": "3440px",
+			},
+		},
 	},
 	plugins: [],
 };
