3708c77c615b43a489ae5cdac91b099a249416de
Zach <zachhutz0@gmail.com>
2024-05-10 14:11:57 -0400

    added search

---
+++ StockSim.Frontend/Pages/Search.razor.cs
@@ -0,0 +1,29 @@
+using StockSim.Frontend.Models;
+
+namespace StockSim.Frontend.Pages {
+    public partial class Search {
+        private string searchTerm = "";
+        private List<CompanyModel> stocks = new();
+        private List<CompanyModel> filteredStocks {
+            get {
+                if (string.IsNullOrWhiteSpace(searchTerm)) {
+                    return stocks.OrderBy(s => s.Name, StringComparer.OrdinalIgnoreCase).ToList();
+                } else {
+                    return stocks.Where(s => s.Name.ToLower().Contains(searchTerm.ToLower()) || s.Ticker.ToLower().Contains(searchTerm.ToLower())).OrderBy(s => s.Name, StringComparer.OrdinalIgnoreCase).ToList();
+                }
+            }
+        }
+        protected override async Task OnInitializedAsync() {
+            base.OnInitialized();
+            var response = await SearchService.GetCompanyList("data.txt");
+            using (var reader = new StreamReader(response)) {
+                string? line;
+                while ((line = await reader.ReadLineAsync()) != null) {
+                    var items = line.Split(',');
+                    stocks.Add(new CompanyModel { Name = items[0], Ticker = items[1] });
+                }
+            }
+            StateHasChanged();
+        }
+    }
+}
