fcfaf3e7cc0f8f9bf84d441473b942a20d649683
Zach <zachhutz0@gmail.com>
2024-05-10 14:01:02 -0400

    added smaller breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ["./**/*.html", "./**/*.razor"],
 	theme: {
 		extend: {
 			screens: {
+				xs: "480px",
+				...defaultTheme.screens,
 				"3xl": "1920px",
 				"4xl": "2560px",
 				"5xl": "3440px",
