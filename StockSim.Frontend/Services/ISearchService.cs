a4ab70ca627f35c9cb1916854dca46acb8d85f56
Zach <zachhutz0@gmail.com>
2024-05-11 13:05:12 -0400

    added methods for building company models from the list and individually retrieving company names

--- StockSim.Frontend/Services/ISearchService.cs
+++ StockSim.Frontend/Services/ISearchService.cs
@@ -1,5 +1,9 @@
+using StockSim.Frontend.Models;
+
 namespace StockSim.Frontend.Services {
     public interface ISearchService {
         Task<Stream> GetCompanyList(string path);
+        Task<List<CompanyModel>> BuildCompanyList(string path);
+        Task<string> GetNameByTicker(string path, string ticker);
     }
 }
--- StockSim.Frontend/Services/SearchService.cs
+++ StockSim.Frontend/Services/SearchService.cs
@@ -1,8 +1,26 @@
+using StockSim.Frontend.Models;
+
 namespace StockSim.Frontend.Services {
     public class SearchService : ServiceBase, ISearchService {
         public SearchService(HttpClient http) : base(http) {}
         public async Task<Stream> GetCompanyList(string path) {
             return await GetHttpClient().GetStreamAsync(path);
+        }
+        public async Task<List<CompanyModel>> BuildCompanyList(string path) {
+            var response = await GetCompanyList(path);
+            List<CompanyModel> stocks = new();
+            using (var reader = new StreamReader(response)) {
+                string? line;
+                while ((line = await reader.ReadLineAsync()) != null) {
+                    var items = line.Split(',');
+                    stocks.Add(new CompanyModel { Ticker = items[0], Name = items[1] });
+                }
+            }
+            return stocks;
+        }
+        public async Task<string> GetNameByTicker(string path, string ticker) {
+            var stocks = await BuildCompanyList(path);
+            return stocks.Where(s => s.Ticker.ToLower() == ticker.ToLower()).First().Name;
         }
     }
 }
